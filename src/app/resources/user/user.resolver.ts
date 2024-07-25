409095883e4e31c16ef502f6aa4b0560de80bbab
Volodymyr-Yevsiuk <vov2706@gmail.com>
2024-07-25 15:51:24 +0300

    .

--- src/app/resources/user/user.resolver.ts
+++ src/app/resources/user/user.resolver.ts
@@ -4,6 +4,9 @@ import { AuthGuard } from '../../common/auth/guard/auth.guard';
 import { UserService } from './user.service';
 import { IContextServer } from '../../common/graphql/graphql.module';
 import { UserEntity } from './entity/user.entity';
+import { RequestedFieldsDecorator } from '../../decorators/requested-fields.decorator';
+import { Prisma } from '@prisma/client';
+import { ExceptionHandlerDecorator } from '../../decorators/exception-handler.decorator';
 
 @Resolver()
 export class UserResolver {
@@ -11,7 +14,11 @@ export class UserResolver {
 
   @UseGuards(AuthGuard)
   @Query(() => UserEntity)
-  async getProfile(@Context() ctx: IContextServer) {
-    return this.userService.findOneById(ctx.req.user.id);
+  @ExceptionHandlerDecorator()
+  async getProfile(
+    @Context() ctx: IContextServer,
+    @RequestedFieldsDecorator() fields: Prisma.UserSelect,
+  ) {
+    return this.userService.findOneById(ctx.req.user.id, fields);
   }
 }
--- src/app/resources/user/user.service.ts
+++ src/app/resources/user/user.service.ts
@@ -1,14 +1,18 @@
 import { DatabaseService } from '../../globals/database/database.service';
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UserService {
   constructor(private readonly db: DatabaseService) {}
 
-  async findOneById(id: number) {
+  async findOneById(id: number, fields: Prisma.UserSelect) {
     return this.db.user.findUniqueOrThrow({
       where: {
         id: id,
+      },
+      select: {
+        ...fields,
       },
     });
   }
