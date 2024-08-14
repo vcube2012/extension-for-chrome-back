67c2f7a29736d63bae041ef21d4cb4cc9fc1964d
Volodymyr-Yevsiuk <vov2706@gmail.com>
2024-08-14 14:14:29 +0300

    .

--- prisma/seeds/seeders/index.ts
+++ prisma/seeds/seeders/index.ts
@@ -1,4 +1,4 @@
+// import PackageSeeder from './package.seeder';
 import { SeederInterface } from './interfaces/seeder.interface';
-import PackageSeeder from './package.seeder';
 
-export const seeders: Array<SeederInterface> = [new PackageSeeder()];
+export const seeders: Array<SeederInterface> = [];
