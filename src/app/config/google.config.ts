9f8c773956185aedf0b84471b0e602a4ac19da98
Volodymyr-Yevsiuk <vov2706@gmail.com>
2024-10-18 14:02:12 +0300

    google

--- src/app/config/google.config.ts
+++ src/app/config/google.config.ts
@@ -4,7 +4,6 @@ import * as process from 'node:process';
 export default registerAs('google', () => ({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  // callbackURL: process.env.GOOGLE_CALLBACK_URL,
   callbackURL: 'http://localhost:3000',
   scopes: [
     'https://www.googleapis.com/auth/userinfo.profile',
